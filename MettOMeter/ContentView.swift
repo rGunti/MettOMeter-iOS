ccecc8598c2714b95ada7b53afac3ec3e8ef5faf
Raphael Guntersweiler <raphael@guntersweiler.net>
2019-11-25 20:53:41 +0100

    Initial Commit

---
+++ MettOMeter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MettOMeter
+//
+//  Created by Raphael Guntersweiler on 25.11.19.
+//  Copyright © 2019 Raphael Guntersweiler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
